999ae7809839fddf0816b35a82ec2c2efe8ccc4d
Gergo5578 <94042625+Gergo5578@users.noreply.github.com>
2025-04-09 13:00:51 +0200

    Delete old files

--- Voluntree/connect.php
+++
@@ -1,5 +0,0 @@
-<?php
-$pdo = new PDO("mysql:host=localhost;dbname=Voluntree", "root", "");
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-?>
-
--- Voluntree/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-session_destroy(); // Session törlése
-header("Location: login.php"); // Visszairányítás a bejelentkezés oldalra
-exit();
-?>
